6cf24af86037d042c139a91c3bb80242978403b4
Abhishek Kumar <theabhishek245@gmail.com>
2025-07-14 22:22:13 +0530

    Add files via upload

---
+++ components/footer.php
@@ -0,0 +1,41 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <h3>Quick links</h3>
+         <a href="home.php"> <i class="fas fa-angle-right"></i> Home</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> About</a>
+         <a href="shop.php"> <i class="fas fa-angle-right"></i> Shop</a>
+         <a href="contact.php"> <i class="fas fa-angle-right"></i> Contact</a>
+      </div>
+
+      <div class="box">
+         <h3>Extra links</h3>
+         <a href="user_login.php"> <i class="fas fa-angle-right"></i> Login</a>
+         <a href="user_register.php"> <i class="fas fa-angle-right"></i> Register</a>
+         <a href="cart.php"> <i class="fas fa-angle-right"></i> Cart</a>
+         <a href="orders.php"> <i class="fas fa-angle-right"></i> Orders</a>
+      </div>
+
+      <div class="box">
+         <h3>Contact Us.</h3>
+         <a href="tel:+91 6398639017"><i class="fas fa-phone"></i> +91 6398639017</a>
+         <a href="tel:+91 6398639017"><i class="fas fa-phone"></i> +91 6398639017</a>
+         <a href="mailto:theabhishek245@gmail.com"><i class="fas fa-envelope"></i> theabhishek245@gmail.com</a>
+         <a href="https://www.google.com/myplace"><i class="fas fa-map-marker-alt"></i> Bareilly, Uttar Pradesh, INDIA </a>
+      </div>
+
+      <div class="box">
+         <h3>Follow Us</h3>
+         <a href="https://www.facebook.com/harshchaudharynp" target="_blank"><i class="fab fa-facebook-f"></i>facebook</a>
+         <a href="https://twitter.com/HarshCh53587355" target="_blank"><i class="fab fa-twitter"></i>Twitter</a>
+         <a href="https://www.instagram.com/its_abhishekkumar05/" target="_blank"><i class="fab fa-instagram"></i>Instagram</a>
+         <a href="https://www.linkedin.com/in/abhishek-kumar-596174230/" target="_blank"><i class="fab fa-linkedin"></i>Linkedin</a>
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; copyright@ <?= date('Y'); ?> by <span>ABHISHEK KUMAR</span> | all rights reserved!</div>
+
+</footer>
